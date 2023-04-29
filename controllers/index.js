67d9c525a5d67c5cc0dbbd3d9b8ff6c41b8b0f8d
Joona21010 <joona21010@student.hamk.fi>
2023-04-29 21:10:14 +0300

    posts on index

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 const router = express.Router();
 
-const home = (req,res) => {
+const dbURI = 'mongodb+srv://'+ process.env.DBUSER +':'+ process.env.DBPASSWD +''+ process.env.CLUSTER +'.mongodb.net/'+ process.env.DB +'?retryWrites=true&w=majority'
+ mongoose.connect(dbURI);
+const Post = require('../models/post.js');
+
+const home = async (req,res) => {
+    /*const posts = await Post.find({});
     res.render('index',
     {
-        pagetitle : "Etusivu"
+        pagetitle : "Etusivu",
+        posts
     });
+*/
+    try {
+        const posts = await Post.find({});
+        res.render('index', { posts });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Server error');
+    }
 }
 
 module.exports = {
